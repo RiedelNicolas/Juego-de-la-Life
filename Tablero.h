b55c1296b0a313efcbbb43c2bfe350ab06358f12
Hugo Larrea <43705596+hugolarreab@users.noreply.github.com>
2018-10-14 23:14:49 -0300

    Create Tablero.h

---
+++ Tablero.h
@@ -0,0 +1,20 @@
+#ifndef TABLERO_H_
+#define TABLERO_H_
+
+#include <iostream>
+#include <string>
+
+class Tablero {
+	private:
+		std::string nombre;
+		int ancho;
+		int alto;
+	public:
+
+
+};
+
+
+
+
+#endif /* TABLERO_H_ */
