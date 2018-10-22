c19612f24dc61511133a227526433468a782216a
gonza-ms <gonzalomartinezsastre@gmail.com>
2018-10-22 18:54:48 -0300

    Añado Archivo.h

---
+++ src/Archivo.h
@@ -0,0 +1,17 @@
+#ifndef ARCHIVO_H_
+#define ARCHIVO_H_
+
+#include <string>
+
+class Archivo{
+	private:
+		std::string ruta;
+	public:
+		Archivo(std::string);
+		~Archivo();
+
+};
+
+
+
+#endif /* ARCHIVO_H_ */
