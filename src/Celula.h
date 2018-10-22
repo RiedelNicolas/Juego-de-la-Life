0446884f44ab8dd3d41f8c1a640611a79b2d88cf
Hugo <you@example.com>
2018-10-22 18:50:02 -0300

    Se agregaron los metodos basicos de Celula

--- src/Celula.h
+++ src/Celula.h
@@ -10,9 +10,21 @@ class Celula{
 		float vida;
 		//Rgb color;
 	public:
+
+		// Post: Se crea una celula muerta y con 0.0 de vida
 		Celula();
-		bool getEstaViva;
-		float getVida;
+
+		// Post: Devuelve un bool que responde a la pregunta "Esta viva?"
+		bool saberSiEstaViva();
+
+
+		void modificarEstado(bool);
+
+
+		float conseguirVida();
+
+
+		void modificarVida(float);
 		//Rgb getColor();
 };
 
