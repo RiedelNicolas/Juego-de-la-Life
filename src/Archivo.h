c020eedb3c4cca651cdd53d5f4b2886e04daa514
RiedelNicolas <nariedel99@gmail.com>
2018-10-25 00:04:47 -0300

    Necesito saber como va a estar implementado el tablero, solo falta eso.

--- src/Archivo.h
+++ src/Archivo.h
@@ -1,19 +1,20 @@
 
 #ifndef ARCHIVO_H_
 #define ARCHIVO_H_
-//skere dijo el duketo
 #include <fstream>
 #include <iostream>
 
 #include "Malla.h"
+#include "Tablero.h"
+#include "Rgb.hpp"
+#include "Celula.h"
 
 class Archivo{
 	private:
 		std::ifstream file;
 		std::string ruta;
 		unsigned cantidadDeMallas;
-		Malla* mallas;
-
+		Tablero* tablero;
 	public:
 
 		/*pre: Recibe el path donde esta ubicado el archivo en forma de string.
@@ -24,15 +25,24 @@ class Archivo{
 		/*post :devuelve la cantidad de tableros que se pudo levantar del archivo*/
 		unsigned getCantidadDeMallas();
 
-		/*post : devuelve la direccion al primer elemento del array de tableros*/
-		 Malla* getMallas();
+		/*post : devuelve un pointer a tablero, si no habia archivos en el tablero devuelve NULL*/
+		 Tablero* getTablero();
 
 		//destructor
 		~Archivo();
 	private:
 		/*crea dicho tableros dinamicos y asigna la direccion de memoria correspondiente a el inicio del array a "tableros"*/
-		void crearTableros();
+		void crearTablero();
+
+		/*cuenta cuantas veces aparece la palabra "Tablero" en el archivo.*/
 		unsigned contarCantidadDeMallas();
+
+		void levantarTablero();
+		/*levanta una malla del archivo, que en el archivo se llaman "Tablero".*/
+		void levantarMalla();
+		void levantarPortal();
+		void levantarParcela();
 };
+
 
 #endif /* ARCHIVO_H_ */
