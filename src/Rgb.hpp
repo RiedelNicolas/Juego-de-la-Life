20b9040bff15a3af85589b5a5844302b19da4c4a
RiedelNicolas <nariedel99@gmail.com>
2018-10-24 22:37:54 -0300

    Rgb o muerte

---
+++ src/Rgb.hpp
@@ -0,0 +1,39 @@
+/*
+ * Rgb.hpp
+ *
+ *  Created on: Oct 12, 2018
+ *      Author: riedel
+ */
+
+#ifndef RGB_HPP_
+#define RGB_HPP_
+
+#define CANTIDAD_COLORES 3
+class Rgb{
+private:
+	int colores[CANTIDAD_COLORES];
+public:
+	/*constructor,
+	 * pre : recibe los 3 enteros correspondiente a los valores del Rgb. Rango(0-255), inicializa en 0 si no se reciben parametros.
+	 * post:asigna los valores recibidos a el RGB, luego de crear el mismo.
+	 */
+	Rgb(int rojo ,int verde ,int azul);
+
+	/*pre: recibe un valor entero en el rango (0-255)
+	 * post: asigna dicho valor a el color correspondiente del Rgb.
+	 */
+	void setRojo(int);
+	void setVerde(int);
+	void setAzul(int);
+
+	/*post: Devuelve un entero correspondiente al valor de dicho color en el RGB.
+	 */
+	int getRojo();
+	int getVerde();
+	int getAzul();
+
+};
+
+
+
+#endif /* RGB_HPP_ */
