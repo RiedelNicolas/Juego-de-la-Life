d3dba17ba694af16f1d4d2dfc2c2ff26cd809d34
gonza-ms <gonzalomartinezsastre@gmail.com>
2018-10-26 15:40:27 -0300

    Modularizo actualizar tablero

--- src/Juego.cpp
+++ src/Juego.cpp
@@ -1,10 +1,9 @@
 #include "Juego.h"
 
-void Juego::actualizarMalla(Tablero* tablero){
+void Juego::actualizarTablero(Tablero* tablero){
 	int filas, columnas, celulasVivasLindantes;
 	celulasVivasTurnoAnterior = 0;
 	cantidadDeCelulasVivas = 0;
-	bool estaViva;
 	Malla* malla = tablero->getMalla();
 
 	while(malla){ //REVISAR CUANDO TABLERO.H ESTÉ TERMINADO
@@ -13,21 +12,28 @@ void Juego::actualizarMalla(Tablero* tablero){
 		filas = malla->getCantidadDeFilas();
 		columnas = malla->getCantidadDeColumnas();
 
-		for(int i=0; i<filas; i++){
-			for(int j=0; j<columnas; j++){
-				celulasVivasLindantes = malla->contarCelulasVivasLindantes(i, j);
-				estaViva = malla->getParcela(i, j)->getEstadoDeCelula();
+		actualizarMalla(filas, columnas, malla);
 
-				if(estaViva && (celulasVivasLindantes<2 || celulasVivasLindantes>3)){
-					malla->getParcela(i, j)->reducirVidaDeCelula();
-				}
-				else if(!estaViva && celulasVivasLindantes==3){
-					malla->getParcela(i, j)->setCelulaNacida();
-				}
-			}
-		}
 		cantidadDeCelulasVivas += malla->getCantidadDeCelulasVivas();
 		malla->getSiguienteMalla();
 	}
 }
 
+void Juego::actualizarMalla(int filas, int columnas, Malla* malla){
+
+	bool estaViva;
+
+	for(int i=0; i<filas; i++){
+		for(int j=0; j<columnas; j++){
+			celulasVivasLindantes = malla->contarCelulasVivasLindantes(i, j);
+			estaViva = malla->getParcela(i, j)->getEstadoDeCelula();
+
+			if(estaViva && (celulasVivasLindantes<2 || celulasVivasLindantes>3)){
+				malla->getParcela(i, j)->reducirVidaDeCelula();
+			}
+			else if(!estaViva && celulasVivasLindantes==3){
+				malla->getParcela(i, j)->setCelulaNacida();
+			}
+		}
+	}
+}
--- src/Juego.h
+++ src/Juego.h
@@ -7,14 +7,18 @@ class Juego{
 	private:
 		int cantidadDeCelulasVivas;
 		int celulasVivasTurnoAnterior;
+
 	public:
 		/* Pre: el tablero está creado.
-		 * Post: Modifica el estado de las células teniendo en cuenta los índices
+		 * Post: Modifica el estado de todas las células del juego teniendo en cuenta los índices
 		 * 		 de natalidad y mortalidad de cada parcela, así como los siguientes preceptos:
 		 * 		 Una célula muerta con 3 células vecinas vivas, nace.
 		 * 		 Una célula viva con 2 o 3 células vecinas vivas, permanece en ese estado.
 		 */
-		void actualizarMalla(Tablero* tablero);
+		void actualizarTablero(Tablero* tablero);
+
+	private:
+		void actualizarMalla(int filas, int columnas, Malla* malla);
 };
 
 
