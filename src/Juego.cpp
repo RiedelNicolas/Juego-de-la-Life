cf7b61552ce2e74bff62627ef781ab4f825bc7c8
gonza-ms <gonzalomartinezsastre@gmail.com>
2018-10-26 17:10:46 -0300

    Arreglo Interfaz

--- src/Juego.cpp
+++ src/Juego.cpp
@@ -1,4 +1,12 @@
 #include "Juego.h"
+using namespace std;
+
+Juego::Juego(){
+	estadoDeJuego = 'C';
+	turno = 0;
+	celulasVivasTurnoAnterior = 0;
+	cantidadDeCelulasVivas = 0;
+}
 
 void Juego::actualizarTablero(Tablero* tablero){
 	int filas, columnas;
@@ -20,7 +28,7 @@ void Juego::actualizarTablero(Tablero* tablero){
 }
 
 void Juego::actualizarMalla(int filas, int columnas, Malla* malla){
-	int contarCelulasLindantes;
+	int celulasVivasLindantes;
 	bool estaViva;
 
 	for(int i=0; i<filas; i++){
@@ -37,3 +45,55 @@ void Juego::actualizarMalla(int filas, int columnas, Malla* malla){
 		}
 	}
 }
+
+
+void Juego::nuevoTurno(){
+	int cantidadDeTurnos;
+	cantidadDeTurnos = pedirCantidadDeTurnos();
+	while(!turnoValido(cantidadDeTurnos)){
+		cout<< "CANTIDAD DE TURNOS NO VALIDA"<<endl;
+		cantidadDeTurnos = pedirCantidadDeTurnos();
+	}
+	ejecutarTurnos(cantidadDeTurnos);
+}
+
+void Juego::reiniciarJuego(){
+	estadoDeJuego = 'R';
+	turno = 0;
+}
+
+void Juego::finalizarJuego(){
+	estadoDeJuego = 'T';
+}
+
+
+bool Juego::cantidadDeTurnoEsValida(int cantidadDeTurnos){
+	return cantidadDeTurnos > 0;
+}
+
+int Juego::pedirCantidadDeTurnos(){
+	int turno;
+	cout <<"Ingrese la cantidad de turnos a ejecutar:";
+	cin >> turno;
+	return turno;
+}
+
+void Juego::ejecutarTurnos(int cantidadDeTurnos){
+	for(int i=0; i<cantidadDeTurnos; i++){
+		//EJECUTAR NUEVO TURNO
+		turno ++;
+	}
+}
+
+float Juego::calcularPromedio(int numero){
+	float promedio;
+	if(turno ==0){
+		promedio = 0;
+	}
+	else{
+		promedio = float(numero)/float(turno);
+	}
+	return promedio;
+}
+
+
