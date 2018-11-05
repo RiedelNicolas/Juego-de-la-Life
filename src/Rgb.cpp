3751bc2b1165cca46349aff225a78065a19c2572
RiedelNicolas <nariedel99@gmail.com>
2018-11-05 00:47:46 -0300

    manteca

--- src/Rgb.cpp
+++ src/Rgb.cpp
@@ -1,60 +1,57 @@
 
 #include "Rgb.hpp"
 
-#define POS_ROJO 0
-#define POS_VERDE 1
-#define POS_AZUL 2
+
 #define MIN_RGB 0
 #define MAX_RGB 255
-#define COLOR_DEFECTO 128
 
 /*pre: recibe un int, correspondiente al valor de un color.
  * post:si el valor del color no se encuentra en el rango, tira una excepción . Siendo el rango (MIN_RGB, MAX_RGB)
  */
+
 void verificarRango(int color);
 
 Rgb::Rgb(int rojoIngresado, int verdeIngresado, int azulIngresado){
 	verificarRango(rojoIngresado);
 	verificarRango(verdeIngresado);
 	verificarRango(azulIngresado);
-	rojo=rojoIngresado;
-	azul=azulIngresado;
-	verde=verdeIngresado;
+	this->rojo = rojoIngresado;
+	this->azul = azulIngresado;
+	this->verde = verdeIngresado;
 }
 
 Rgb::Rgb(){
-	rojo=COLOR_DEFECTO;
-	azul=COLOR_DEFECTO;
-	verde=COLOR_DEFECTO;
+	this->rojo=COLOR_POR_DEFECTO;
+	this->azul=COLOR_POR_DEFECTO;
+	this->verde=COLOR_POR_DEFECTO;
 }
 
 void Rgb:: setRojo(int colorIngresado){
 	verificarRango(colorIngresado);
-	rojo=colorIngresado;
+	this->rojo=colorIngresado;
 }
 
 void Rgb:: setVerde(int colorIngresado){
 	verificarRango(colorIngresado);
-	verde=colorIngresado;
+	this->verde=colorIngresado;
 }
 
 void Rgb:: setAzul(int colorIngresado){
 	verificarRango(colorIngresado);
-	azul=colorIngresado;
+	this->azul=colorIngresado;
 }
 
 int Rgb:: getRojo(){
-	return rojo;
+	return (this->rojo);
 }
 
 int Rgb:: getVerde(){
-	return verde;
+	return (this->verde);
 }
 
 int Rgb:: getAzul(){
-	return azul;
+	return (this->azul);
 }
-
 
 void verificarRango(int color){
 	if( !(color<=MAX_RGB && color>=MIN_RGB) ){
