6c693e4236e3b1c112f8d37513a087fe9c725086
ezezbogar <ezezbogar@gmail.com>
2018-11-29 23:52:59 -0300

    Corrijo Grafo

--- src/Grafo.cpp
+++ src/Grafo.cpp
@@ -1,16 +1,18 @@
 #include "Grafo.h"
 
 Grafo::Grafo(int dimension){
-	cantidadDeVertices = dimension;
-	adyacencias = new int[dimension];
 
-	for(int i=0; i<dimension; i++){
+	cantidadDeVertices = dimension;
+	adyacencias = new int*[dimension];
+
+	for(int i = 0; i < dimension; i++){
 		adyacencias[i] = new int*[dimension];
 	}
 }
 
 Grafo::~Grafo(){
-	for(int i=0; i<dimension; i++){
+
+	for(int i = 0; i < cantidadDeVertices; i++){
 		delete[] adyacencias[i];
 	}
 	delete[] adyacencias;
